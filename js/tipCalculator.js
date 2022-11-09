bcbdc0fab8fd989b2e814e8efd447a2834f07ff6
David Lara <david.a.lara05@gmail.com>
2022-11-09 13:41:32 -0600

    inialize repo

---
+++ js/tipCalculator.js
@@ -0,0 +1,26 @@
+
+// I am getting the bill input
+
+$("#billTotalInput").click(function (){
+    alert("hello")
+});
+
+// I am getting the tip input
+
+$("#tipInput").click(function (){
+    alert(("whatup"))
+})
+
+
+// buttons
+
+$(".buttonText").click(function (){
+    alert("itwork")
+})
+
+
+// Total
+
+$("#perPersonTotal").hover(function (){
+    // alert("total")
+})
