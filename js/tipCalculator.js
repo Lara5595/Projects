16f91aa7f6e036b5304d2f90d3af35af6ce61248
David Lara <david.a.lara05@gmail.com>
2022-11-12 17:54:09 -0600

    added console.logs

--- js/tipCalculator.js
+++ js/tipCalculator.js
@@ -1,8 +1,10 @@
 // I am creating variables so we can access the inputs and buttons
 
 const billInput = document.getElementById("billTotalInput");
+console.log(billInput)
 
 const tipInput = document.getElementById("tipInput")
+console.log(tipInput)
 
 const numberOfPeopleDiv = document.getElementById("numberOfPeople")
 
@@ -17,7 +19,6 @@ console.log(numberOfPeople)
 
 function calculateBill(){
     const bill = Number(billInput.value)
-
     const tipPercentage = Number(tipInput.value) / 100;
     const total = bill + tipPercentage;
     console.log(total)
